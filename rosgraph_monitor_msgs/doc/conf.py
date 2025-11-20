be6580fd152f2e0f5939f7d36d94b597b2d69867
shrujan <shrujanus@gmail.com>
2025-11-20 01:36:32 +0000

    docs: standardize quotes in html_theme_options in conf.py files
    Signed-off-by: shrujan <shrujanus@gmail.com>

--- rosgraph_monitor_msgs/doc/conf.py
+++ rosgraph_monitor_msgs/doc/conf.py
@@ -17,7 +17,7 @@ extensions = ['myst_parser']
 
 html_theme = 'alabaster'
 html_theme_options = {
-    "collapse_navigation": False,
-    "fixed_sidebar": True,
-    "navigation_depth": 4,
+    'collapse_navigation': False,
+    'fixed_sidebar': True,
+    'navigation_depth': 4,
 }
